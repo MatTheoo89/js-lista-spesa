c353ee81a3026f4da83ccb6f7123dcc257045b79
MatTheoo89 <matteobellomo89@gmail.com>
2022-10-18 15:40:10 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+console.log('link ok');
+
+const listaSpesa = [
+  'Caffè',
+  'Latte',
+  'Frutta',
+  'Pasta',
+  'Passata di pomodoro',
+  'Carne di manzo',
+  'Pollo',
+  'Bibite',
+  'Birra',
+  'Cereali',
+  'Frutta secca',
+]
+// console.log(listaSpesa);
+
+const ul = document.createElement('ul');
+const container = document.querySelector('.container');
+console.log(ul);
+
+
+for(let i = 0; i < listaSpesa.length; i++){
+  
+  // creo variabile che racchiude i singoli prodotti
+  let alimento = listaSpesa[i];
+
+  // crea variabile LI
+  let li = document.createElement('li');
+  console.log(li);
+
+  // stampa i singoli prodotti in una variabile LI
+  li.innerText = alimento;
+  console.log(li.innerText);
+  
+  // appendi gli LI ad UL
+  ul.append(li);
+  
+  //appendi UL a container
+  ul.append(container);
+  
+
+
+};
+
+
+
+
+
+
